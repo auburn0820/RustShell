4e89ab4b91569c9e4d8d13b43e8e5fe39ad51036
피수영 <pisuyeong@pisuyeong-ui-Mac-Studio.local>
2023-05-09 22:22:24 +0900

    first commit

---
+++ target/debug/RustShell.d
@@ -0,0 +1,1 @@
+/Users/pisuyeong/Desktop/Dev/Rust/RustShell/target/debug/RustShell: /Users/pisuyeong/Desktop/Dev/Rust/RustShell/src/main.rs
---
+++ target/debug/deps/RustShell-a63b43d18f409c76.d
@@ -0,0 +1,5 @@
+/Users/pisuyeong/Desktop/Dev/Rust/RustShell/target/debug/deps/RustShell-a63b43d18f409c76: src/main.rs
+
+/Users/pisuyeong/Desktop/Dev/Rust/RustShell/target/debug/deps/RustShell-a63b43d18f409c76.d: src/main.rs
+
+src/main.rs:
